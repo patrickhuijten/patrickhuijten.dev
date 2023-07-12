1f08a9698465ddc15980aab1282a2ff0581845a8
Patrick Huijten <patrick.huijten@ingka.ikea.com>
2023-07-12 18:30:56 +0200

    style: corrected bio panel height

--- next/src/app/views/about/social.tsx
+++ next/src/app/views/about/social.tsx
@@ -5,26 +5,28 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "@/app/views/about/social.module.css";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 
 type SocialProps = {
   src: string;
   iconType: "linkedin" | "artstation" | "github";
 };
 export const Social = ({ src, iconType }: SocialProps) => {
+  const icon = getIcon(iconType);
   return (
-    <a href={src} target="_blank">
-      <FontAwesomeIcon className={styles.social} icon={getIcon(iconType)} />
+    <a className={styles.link} href={src} target="_blank">
+      <FontAwesomeIcon className={styles.social} icon={icon} />
     </a>
   );
 };
 
-const getIcon = (iconType: SocialProps["iconType"]) => {
+const getIcon = (iconType: SocialProps["iconType"]): IconProp => {
   switch (iconType) {
     case "linkedin":
-      return faLinkedin;
+      return faLinkedin as IconProp;
     case "artstation":
-      return faArtstation;
+      return faArtstation as IconProp;
     case "github":
-      return faGithub;
+      return faGithub as IconProp;
   }
 };
