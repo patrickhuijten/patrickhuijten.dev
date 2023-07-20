7eeb5b960c6653d9f5b326758f3ab74d38f49e4a
Patrick Huijten <patrick.huijten@ingka.ikea.com>
2023-07-20 22:21:31 +0200

    chore: override vue project

--- helpers/renderRichText.js
+++
@@ -1,6 +0,0 @@
-export const renderRichText = (text, storyapi) => {
-    let html = text ? storyapi.richTextResolver.render(text) : ''
-
-    html = html.replaceAll('<a ', '<a rel="noreferrer" ')
-    return html
-}
